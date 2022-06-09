fd0c3b843d7f3df3dc19f2c6d6a927e14897929c
luischo04 <83773720+luischo04@users.noreply.github.com>
2022-06-09 08:46:31 -0500

    Update registrar.php
    actualizacion

--- Views/Modulos/registrar.php
+++ Views/Modulos/registrar.php
@@ -4,23 +4,23 @@
             <form class="form" method="POST" action="">
                 <div class="col-12">
                     <label for="idEmpleado" class="form-label">Id</label>
-                    <input type="text" class="form-control" id="idEmpleado" name="idEmpleadoR" >
+                    <input type="text" class="form-control" id="idEmpleado" name="idEmpleadoR" required>
                 </div>
                 <div class="col-12">
                     <label for="nombre" class="form-label">Nombre</label>
-                    <input type="text" class="form-control" id="nombre" name="nombreR">
+                    <input type="text" class="form-control" id="nombre" name="nombreR" required>
                 </div>
                 <div class="col-12">
                     <label for="apePaterno" class="form-label">Apellido Paterno</label>
-                    <input type="text" class="form-control" id="apePaterno" name="apePaternoR">
+                    <input type="text" class="form-control" id="apePaterno" name="apePaternoR" required>
                 </div>
                 <div class="col-12">
                     <label for="apeMaterno" class="form-label">Apellido Materno</label>
-                    <input type="text" class="form-control" id="apeMaterno" name="apeMaternoR">
+                    <input type="text" class="form-control" id="apeMaterno" name="apeMaternoR" required>
                 </div>
                 <div class="col-12">
                     <label for="fechaNacimiento" class="form-label">Fecha de nacimiento</label>
-                    <input type="date" class="form-control" id="fechaNacimiento" name="fechaNacimientoR">
+                    <input type="date" class="form-control" id="fechaNacimiento" name="fechaNacimientoR" required>
                 </div>
                 <div class="col-12">
                     <button type="submit" class="btn btn-primary">Guardar</button>
